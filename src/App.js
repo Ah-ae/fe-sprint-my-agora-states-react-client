5cc2c81b92ff2306ed8bf892e36f32c0bff4beab
Ah-ae <kimaae@gmail.com>
2023-01-30 15:29:38 +0900

    graphql로 데이터 받아오기 구현

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
+import { graphql } from "@octokit/graphql";
 import { useState, useEffect } from "react";
 import DiscussionList from "./components/DiscussionList";
 import Form from "./components/Form";
 // import { Form, DiscussionList } from "./components";
 
 function App() {
-  const domain = "http://localhost:4000";
   const [agoraStatesDiscussions, setAgoraStatesDiscussions] = useState([]);
 
-  const getAgoraStatesDiscussions = async () => {
-    const response = await fetch(
-      domain +
-        "/discussions?" +
-        new URLSearchParams({
-          limit: 10,
-          page: 3,
-        })
-    );
-    const data = await response.json();
-    setAgoraStatesDiscussions(data);
+  // * graphQL로 실시간 데이터 받아오기
+  const query = `
+  query {
+    repository(owner: "codestates-seb", name: "agora-states-fe") {
+      discussions (first: 20) {
+        edges {
+          node {
+            id
+            createdAt
+            title
+            url
+            author {
+              login
+            }
+            answer {
+              id
+              createdAt
+              url
+              author {
+                login
+              }
+              bodyHTML
+            }
+            bodyHTML
+          }
+        }
+      }
+    }
+    viewer {
+      login
+    }
+  }
+  `;
+  const options = {
+    headers: {
+      authorization: "token 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",
+    },
   };
+
+  async function getAgoraStatesDiscussions() {
+    const { repository } = await graphql(query, options);
+    setAgoraStatesDiscussions(repository.discussions.edges);
+  }
 
   useEffect(() => {
     getAgoraStatesDiscussions();
   }, []);
+  console.log(agoraStatesDiscussions);
+
+  // * fetch
+  const domain = "http://localhost:4000";
+
+  // const getAgoraStatesDiscussions = async () => {
+  //   const response = await fetch(
+  //     domain +
+  //       "/discussions?" +
+  //       new URLSearchParams({
+  //         limit: 10,
+  //         page: 1,
+  //       })
+  //   );
+  //   const data = await response.json();
+  //   setAgoraStatesDiscussions(data);
+  // };
+
+  // useEffect(() => {
+  //   getAgoraStatesDiscussions();
+  // }, []);
 
   const addDiscussion = ({ title, author, bodyText }) => {
     const newDiscussionData = {
